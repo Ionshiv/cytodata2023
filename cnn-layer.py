7cfca406ea4f0c88499d8c76cb321b61d851095e
Ionshiv <david.holmberg@farmbio.uu.se>
2021-04-22 15:18:38 +0200

    defining classes

---
+++ cnn-layer.py
@@ -0,0 +1,15 @@
+
+
+class cnn-layer:
+
+    def __init__(self, dimension):
+        msg(True)
+        msg(False)
+    
+    def msg(self, bvar: bool):
+        if bvar:
+            print("+++DEFINING CNN LAYER+++")
+        else:
+            print("+++LAYER DEFINED+++")
+            
+
--- data-augmenter.py
+++ data-augmenter.py
@@ -1,1 +1,17 @@
 
+
+
+class data-augmenter:
+
+    def __init__(self):
+        msg(True)
+    
+    def msg(self, bool_var: bool):
+
+        if bool_var:
+            print("+++INNITIALIZING DATA AUGMENTER+++")
+        else:
+            print("+++TERMINATING DATA AUGMENTER+++")
+        
+    
+
