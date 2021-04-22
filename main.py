f8dd4ceeb608b38342f89ea9afa90300522d1f07
Ionshiv <david.holmberg@farmbio.uu.se>
2021-04-22 14:12:51 +0200

    test4

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 
+
+
+def main():
+    handshake()
+    endshake()
+
+def handshake():
+    print("+++HELLO+++")
+
+def endshake():
+    print("+++ENDING SESSION+++")
+
+
+if __name__ == "__main__":
+    main()
