1d5a72be1845e788d805705a26d2343ae8bf12c7
David Holmberg <david.holmberg@farmbio.uu.se>
2021-08-30 10:43:32 +0200

    Restructure of project

---
+++ JavaLCP/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("+++STARTING IMAGEJ MACRO+++");
+        System.out.println("+++MACRO RUN COMPLETE+++");
+    }
+}
