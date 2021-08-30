fcaaa3d40cda9c2e7024adc73c10bbae9efc0460
David Holmberg <david.holmberg@farmbio.uu.se>
2021-08-30 13:07:44 +0200

    reset

--- JavaLCP/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("+++STARTING IMAGEJ MACRO+++");
-        System.out.println("+++MACRO RUN COMPLETE+++");
-    }
-}
