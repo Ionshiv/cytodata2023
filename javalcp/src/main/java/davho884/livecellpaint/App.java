ef8930623278a7c94a0acb7af1e3cbd484bbc013
David Holmberg <david.holmberg@farmbio.uu.se>
2021-08-31 09:46:16 +0200

    update on tracking project

---
+++ javalcp/src/main/java/davho884/livecellpaint/App.java
@@ -0,0 +1,13 @@
+package davho884.livecellpaint;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ javalcp/src/test/java/davho884/livecellpaint/AppTest.java
@@ -0,0 +1,20 @@
+package davho884.livecellpaint;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
