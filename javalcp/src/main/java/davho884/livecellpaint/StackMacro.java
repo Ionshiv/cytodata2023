779382370c5d4cd4b99cc4c67689b2cf0bed6eae
David Holmberg <david.holmberg@farmbio.uu.se>
2021-08-31 15:15:20 +0200

    update 511

---
+++ javalcp/src/main/java/davho884/livecellpaint/StackMacro.java
@@ -0,0 +1,30 @@
+package davho884.livecellpaint;
+import ij.*;
+import java.util.*;
+import java.awt.*;
+import java.io.*;
+import java.lang.reflect.Array;
+
+//This macro generates hyper-stacks from the image series, then saves them as hyper-tiffs
+
+public class StackMacro {
+
+    public StackMacro(){
+
+    }
+
+    public void importSeq(){
+        //imports TIF sequence
+        return;
+    }
+    public void hyperStacking(){
+        //Converts to TIF Hyperstack
+        return;
+
+    }
+    public void saveStack(){
+        //saves Hyperstacks
+        return;
+    }
+    
+}
--- javalcp/src/main/java/davho884/livecellpaint/TrackMacro.java
+++ javalcp/src/main/java/davho884/livecellpaint/TrackMacro.java
@@ -5,17 +5,20 @@ import java.awt.*;
 import java.io.*;
 import java.lang.reflect.Array;
 
-public class CmdMacro {
+//this macro generates the automated tracking
+
+public class TrackMacro {
 
 
 
-    public CmdMacro(){
+    public TrackMacro(){
 
     }
     public void endMsg(){
         System.out.println("+++ENDING MACRO+++");
         return;
     }
+    //loads TIF stack
     public ImagePlus loadImg(String imgPath){
         ij.io.Opener opener = new ij.io.Opener();
         ImagePlus img = new ImagePlus();
