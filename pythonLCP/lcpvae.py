91c673ab0dfdd6bc1b1ce3a315a11785790739ab
David Holmberg --Cluster <David.Holmberg@farmbio.uu.se>
2021-10-28 12:03:54 +0000

    updated model

--- pythonLCP/lcpvae.py
+++ pythonLCP/lcpvae.py
@@ -1,4 +1,5 @@
 from keras.engine.sequential import Sequential
+from keras.layers.core import Dense, Reshape
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +8,7 @@ import pandas as pd
 import skimage as ski
 from tensorflow import keras
 from keras import layers
+from keras.models import Model
 
 
 class LcpVae:
@@ -23,16 +25,47 @@ class LcpVae:
         else:
             print("+++TERMINATING AUTOENCODER+++")
     
-    def reticulum():
+    def autoencoder():
         # encoder = layers.sequential()
         # decoder = layers.sequential()
 
-        inputs = layers.Input(1080, 1080, 5)
-        encoder = layers.Conv2D(padding = 'same', kernel_size=(3,3), activation='relu')(inputs)
+        ingress = layers.Input(1080, 1080, 5)
+        encoder = layers.Conv2D(padding = 'same', kernel_size=(3,3), activation='relu')(ingress)
         encoder = layers.Conv2D(padding = 'same', kernel_size=(3,3), activation='relu')(encoder)
-        encoder = layers.MaxPool2D(pool_size=(2, 2))(encoder)
+        encoder = layers.MaxPool2D(pool_size=(2, 2), strides=2)(encoder)
         encoder = layers.Conv2D(padding = 'same', kernel_size=(3,3), activation='relu')(encoder)
         encoder = layers.Conv2D(padding = 'same', kernel_size=(3,3), activation='relu')(encoder)
+        encoder = layers.MaxPool2D(pool_size=(2,2), strides=2)(encoder)
+        encoder = layers.Conv2D(padding=same, kernel_size=(3,3), activation='relu')(encoder)
+        encoder = layers.Conv2D(padding=same, kernel_size=(3,3), activation='relu')(encoder)
+        encoder = layers.MaxPool2D(pool_size=(2,2), strides=2)(encoder)
+        encoder = layers.Conv2D(padding=same, kernel_size=(3,3), activation='relu')(encoder)
+        encoder = layers.Conv2D(padding=same, kernel_size=(3,3), activation='relu')(encoder)
+
+        transcoder = layers.Flatten()(encoder)
+        transcoder = Dense(100, activation='softmax')(transcoder)
+        transcoder = Dense(20, activation='softmax')(transcoder)
+        recursion = layers.SimpleRNN(2)(transcoder)
+        transcoder = Dense(20, activation='softmax')(recursion)
+        transcoder = Dense(100, activation='softmax')(transcoder)
+        
+        decoder = Reshape((145, 145, 5))(transcoder)
+        decoder = layers.Conv2DTranspose(padding='same', kernel_size=(3,3), activation='relu')(decoder)
+        decoder = layers.Conv2DTranspose(padding='same', kernel_size=(3,3), activation='relu')(decoder)
+        decoder = layers.UpSampling2D(size=(2, 2))(decoder)
+        decoder = layers.Conv2DTranspose(padding='same', kernel_size=(3,3), activation='relu')(decoder)
+        decoder = layers.Conv2DTranspose(padding='same', kernel_size=(3,3), activation='relu')(decoder)
+        decoder = layers.UpSampling2D(size=(2, 2))(decoder)
+        decoder = layers.Conv2DTranspose(padding='same', kernel_size=(3,3), activation='relu')(decoder)
+        decoder = layers.Conv2DTranspose(padding='same', kernel_size=(3,3), activation='relu')(decoder)
+        decoder = layers.UpSampling2D(size=(2, 2))(decoder)
+        decoder = layers.Conv2DTranspose(padding='same', kernel_size=(3,3), activation='relu')(decoder)
+        egress = layers.Conv2DTranspose(padding='same', kernel_size=(3,3), activation='relu')(decoder)
+
+        aemodel = Model(ingress, egress)
+
+
+
 
         # encoder.add(layers.Conv2D(4,
         #     padding = 'same',
